f1e1312282803cc3a05256de3d1ab225e1af6c0d
Lucas <r.lucasmoretto@gmail.com>
2022-03-30 11:09:35 -0300

    add instructions

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,9 +4,12 @@ import GameCards from './GameCards'
 import styles from './Section.module.css'
 
 function Section() {
-    return(
+    return (
         <section className={styles.section}>
             <Container customClass='column'>
+                <p className={styles.instructions}>
+                    Instructions: Choose a character and click it, don't repeat the same character.
+                </p>
                 <GameCards />
             </Container>
         </section>
